77fa812bd0533c6dd40c71456cdb1825923c5448
Filipe da Silva Granja <110956203+filipegranja98@users.noreply.github.com>
2023-11-04 18:26:28 -0300

    Criando os relacionamentos entre as classes

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,3 +1,34 @@
-public class Biblioteca {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+public class Biblioteca {
+     ArrayList<Ebook> ebooks;
+     UsuarioColaborador usuario;
+
+     public void adicionarEbook(Ebook ebook){
+
+          ebooks.add(ebook);
+     }
+     public void excluirEbook(Ebook ebook){
+          ebooks.remove(ebook);
+     }
+     public ArrayList<Ebook> listarEbooksAutorais() {
+          ArrayList<Ebook> ebooksAutorais = new ArrayList<>();
+          for (Ebook ebook : ebooks) {
+               if (ebook instanceof EbooksAutorais) {
+                    ebooksAutorais.add(ebook);
+               }
+          }
+          return ebooksAutorais;
+     }
+
+     public ArrayList<Ebook> listarEbooksAdquiridos() {
+          ArrayList<Ebook> ebooksAdquiridos = new ArrayList<>();
+          for (Ebook ebook : ebooks) {
+               if (ebook instanceof EbooksAdquiridos) {
+                    ebooksAdquiridos.add(ebook);
+               }
+          }
+          return ebooksAdquiridos;
+     }
 }
